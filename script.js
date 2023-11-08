278dd016939db842446746891e0697351d47a3b1
ronyv22 <ronyvic84@gmail.com>
2023-11-07 20:00:04 -0500

    Updated html

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
-/*Declared variables and assigned them to the getElementById method to access and locate the elements from the html document*/
-const count = document.getElementById('count');
-const sub = document.getElementById('sub');
-const plus = document.getElementById('plus');
+/*Build a simple counter*/
 
-/*Set up an addEventListener method for the +/- elements when clicked on will increment/decrement 
-the number value of the count property by 1 and call the function to change color*/
-plus.addEventListener('click', () => {
-  count.innerHTML++;
-  applyColor()
+/*
+
+Step1 create an html doc with a container that will house the p element and button elements +/-
+
+*/
+
+
+
+
+
+
+
+
+/*Defined a variable called count with a value of 0 for the starting point*/
+
+let count = 0;
+
+/*Declared variables and assigned them to the getElementById method to access and locate the elements from the html document*/
+const counterElement = document.getElementById('count');
+const subButtonElement = document.getElementById('sub');
+const plusButtonElement = document.getElementById('plus');
+
+/*I Set up an addEventListener method for the +/- button elements everytime the buttons are clicked.
+The number value of the counter element will increment/decrement by 1 and call the function to change color*/
+plusButtonElement.addEventListener('click', () => {
+  count++;
+  counterElement.textContent = count;
+  applyColor();
 });
 
-sub.addEventListener('click', () => {
-  count.innerHTML--;
-  applyColor()
+subButtonElement.addEventListener('click', () => {
+  count--;
+  counterElement.textContent = count;
+  applyColor();
 });
 
 /*Defined a function that will be called to change the color of the value if the conditional statements are true*/
 function applyColor() {
-  if (count.innerHTML > 0) {
-    count.style.color = '#3bceac';
-  } else if (count.innerHTML < 0) {
-    count.style.color = '#ee4266';
+  if (count > 0) {
+    counterElement.style.color = '#3bceac';
+  } else if (count < 0) {
+    counterElement.style.color = '#ee4266';
   } else {
-    count.style.color = 'black';
+    counterElement.style.color = 'black';
   }
 }
